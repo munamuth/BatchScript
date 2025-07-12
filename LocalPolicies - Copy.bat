9d22681fc14e5ed0889ab18f60d5a6c77771c1fe
munamuth <nemmuth@outlook.com>
2025-07-12 08:43:39 +0700

    Init

--- LocalPolicies - Copy.bat
+++ LocalPolicies - Copy.bat
@@ -5,8 +5,11 @@ reg add "HKCU\Control Panel\Desktop" /v ScreenSaveActive /t REG_SZ /d 1 /f
 reg add "HKCU\Control Panel\Desktop" /v SCRNSAVE.EXE /t REG_SZ /d "C:\Windows\System32\scrnsave.scr" /f
 
 :: Lock account after 3 failed logins
+::Locks the account after 3 incorrect password attempts.
 net accounts /lockoutthreshold:3
+::Account remains locked for 3 minutes.
 net accounts /lockoutduration:3
+::Failed login counter resets after 3 minutes without a failed attempt.
 net accounts /lockoutwindow:3
 
 :: Password policy - change every 3 months
@@ -14,13 +17,7 @@ net accounts /maxpwage:90
 net accounts /minpwage:1
 net accounts /uniquepw:0
 
-:: Enforce password complexity
-echo Enforcing password complexity...
-secedit /configure /db secedit.sdb /cfg "%~dp0complexity.inf" /quiet
-
-:: Create secure admin account
 net user ITAdmin admin@IT /add
 net localgroup administrators ITAdmin /add
-
 echo Local account security policies applied.
 pause
